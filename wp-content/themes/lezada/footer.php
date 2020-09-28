506373567b82f78eb45130813c160b88d6751118
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-09-28 17:41:27 +0300

    правки

--- wp-content/themes/lezada/footer.php
+++ wp-content/themes/lezada/footer.php
@@ -12,76 +12,75 @@
 ?>
 <?php
 
-do_action( 'lezada_main_container_bottom' );
+do_action('lezada_main_container_bottom');
 
-$pageID         = Lezada_Helper::get_the_ID();
-$disable_footer = get_post_meta( $pageID, 'lezada_disable_footer', true );
+$pageID = Lezada_Helper::get_the_ID();
+$disable_footer = get_post_meta($pageID, 'lezada_disable_footer', true);
 
-$footer_width        = lezada_get_option( 'footer_width' );
-$footer_color_scheme = lezada_get_option( 'footer_color_scheme' );
+$footer_width = lezada_get_option('footer_width');
+$footer_color_scheme = lezada_get_option('footer_color_scheme');
 
 $footer_sidebars = array(
-	'sidebar-footer-1',
-	'sidebar-footer-2',
-	'sidebar-footer-3',
-	'sidebar-footer-4',
+    'sidebar-footer-1',
+    'sidebar-footer-2',
+    'sidebar-footer-3',
+    'sidebar-footer-4',
 );
 
-$layout = explode( '_', lezada_get_option( 'footer_layout' ) );
+$layout = explode('_', lezada_get_option('footer_layout'));
 
 ?>
 
 </div>
 
-<?php if ( ! $disable_footer ) : ?>
+<?php if (!$disable_footer) : ?>
 
-	<div id="footer" class="site-footer site-footer--<?php echo esc_attr( $footer_color_scheme ); ?> ">
-		<div class="container<?php echo '' . ( $footer_width == 'wide' ) ? ' wide' : ''; ?>">
-			<div class="row">
+    <div id="footer" class="site-footer site-footer--<?php echo esc_attr($footer_color_scheme); ?> ">
+        <div class="container<?php echo '' . ($footer_width === 'wide') ? ' wide' : ''; ?>">
+            <div class="row">
 
-				<?php
+                <?php
 
-				$columns = $layout[0];
-				$col_xl  = $layout[1];
-				$col_lg  = $columns > 1 ? 4 : 12;
+                $columns = $layout[0];
+                $col_xl = $layout[1];
+                $col_lg = $columns > 1 ? 4 : 12;
 
-				?>
+                ?>
 
-				<?php for ( $i = 0; $i < $columns; $i ++ ) : ?>
-					<div
-						class="footer-column col-xs-12 col-lg-<?php echo esc_attr( $col_lg ); ?> col-xl-<?php echo esc_attr( $col_xl ); ?>">
-						<?php if ( is_active_sidebar( $footer_sidebars[ $i ] ) ) : ?>
-							<?php dynamic_sidebar( $footer_sidebars[ $i ] ); ?>
-						<?php endif; ?>
-					</div>
-				<?php endfor; ?>
+                <?php for ($i = 0; $i < $columns; $i++) : ?>
+                    <div class="footer-column col-xs-12 col-lg-<?php echo esc_attr($col_lg); ?>
+                     col-xl-<?php echo esc_attr($col_xl); ?>">
+                        <?php if (is_active_sidebar($footer_sidebars[$i])) : ?>
+                            <?php dynamic_sidebar($footer_sidebars[$i]); ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
 
-			</div>
-		</div>
+        <?php if (lezada_get_option('footer_copyright')): ?>
+            <div class="site-copyright">
+                <div class="container<?php echo '' . ($footer_width === 'wide') ? ' wide' : ''; ?>">
+                    <div class="row flex-items-xs-middle">
+                        <div class="col-xs-12 text-xs-center">
+                            <?php echo wp_kses_post(lezada_get_option('footer_copyright_text')); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
 
-		<?php if ( lezada_get_option( 'footer_copyright' ) ): ?>
-			<div class="site-copyright">
-				<div class="container<?php echo '' . ( $footer_width == 'wide' ) ? ' wide' : ''; ?>">
-					<div class="row flex-items-xs-middle">
-						<div class="col-xs-12 text-xs-center">
-							<?php echo wp_kses_post( lezada_get_option( 'footer_copyright_text' ) ); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php endif; ?>
-
-	</div>
+    </div>
 <?php endif; ?>
 
-<?php do_action( 'lezada_site_bottom' ); ?>
+<?php do_action('lezada_site_bottom'); ?>
 
 <?php echo Lezada_Templates::back_to_top() ?>
 <?php echo Lezada_Templates::cookie_notice() ?>
 
 </div>
 
-<?php do_action( 'lezada_after_page_container' ); ?>
+<?php do_action('lezada_after_page_container'); ?>
 <?php wp_footer(); ?>
 
 </body>
