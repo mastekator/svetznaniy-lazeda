7910073daae6bfa16c4b408fcefd61d9436c79b9
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-10-01 12:28:44 +0300

    перевод js

--- wp-content/themes/lezada/functions.php
+++ wp-content/themes/lezada/functions.php
@@ -7,80 +7,81 @@
  */
 $lezada_theme = wp_get_theme();
 
-if ( ! empty( $lezada_theme['Template'] ) ) {
-	$lezada_theme = wp_get_theme( $lezada_theme['Template'] );
+if (!empty($lezada_theme['Template'])) {
+    $lezada_theme = wp_get_theme($lezada_theme['Template']);
 }
 
-if ( ! defined( 'DS' ) ) {
-	define( 'DS', DIRECTORY_SEPARATOR );
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
 }
 
-define( 'LEZADA_THEME_NAME', $lezada_theme['Name'] );
-define( 'LEZADA_THEME_SLUG', $lezada_theme['Template'] );
-define( 'LEZADA_THEME_VERSION', $lezada_theme['Version'] );
-define( 'LEZADA_THEME_DIR', get_template_directory() );
-define( 'LEZADA_THEME_URI', get_template_directory_uri() );
-define( 'LEZADA_CHILD_THEME_URI', get_stylesheet_directory_uri() );
-define( 'LEZADA_CHILD_THEME_DIR', get_stylesheet_directory() );
+define('LEZADA_THEME_NAME', $lezada_theme['Name']);
+define('LEZADA_THEME_SLUG', $lezada_theme['Template']);
+define('LEZADA_THEME_VERSION', $lezada_theme['Version']);
+define('LEZADA_THEME_DIR', get_template_directory());
+define('LEZADA_THEME_URI', get_template_directory_uri());
+define('LEZADA_CHILD_THEME_URI', get_stylesheet_directory_uri());
+define('LEZADA_CHILD_THEME_DIR', get_stylesheet_directory());
 
-define( 'LEZADA_LIBS_URI', LEZADA_THEME_URI . '/assets/libs' );
+define('LEZADA_LIBS_URI', LEZADA_THEME_URI . '/assets/libs');
 
-define( 'LEZADA_IMAGES', LEZADA_THEME_URI . '/assets/images' );
-define( 'LEZADA_ADMIN_IMAGES', LEZADA_THEME_URI . '/assets/admin/images' );
+define('LEZADA_IMAGES', LEZADA_THEME_URI . '/assets/images');
+define('LEZADA_ADMIN_IMAGES', LEZADA_THEME_URI . '/assets/admin/images');
 
-define( 'LEZADA_INC_DIR', LEZADA_THEME_DIR . DS . 'includes' );
-define( 'LEZADA_INC_URI', LEZADA_THEME_URI . '/includes' );
-define( 'LEZADA_FRAMEWORK_DIR', LEZADA_THEME_DIR . DS . 'framework' );
-define( 'LEZADA_FRAMEWORK_URI', LEZADA_THEME_URI . '/framework' );
+define('LEZADA_INC_DIR', LEZADA_THEME_DIR . DS . 'includes');
+define('LEZADA_INC_URI', LEZADA_THEME_URI . '/includes');
+define('LEZADA_FRAMEWORK_DIR', LEZADA_THEME_DIR . DS . 'framework');
+define('LEZADA_FRAMEWORK_URI', LEZADA_THEME_URI . '/framework');
 
-define( 'LEZADA_OPTIONS_DIR', LEZADA_INC_DIR . DS . 'theme-options' );
+define('LEZADA_OPTIONS_DIR', LEZADA_INC_DIR . DS . 'theme-options');
 
 /**
  * Load Insight Framework.
  *
  * @since 1.0
  */
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-coming-soon.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-compatible.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-enqueue.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-extras.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-helper.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-import.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-init.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-instagram.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-metabox.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-tgm-plugin-activation.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-plugins.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-sercurity.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-templates.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-walker-nav-menu.php' );
-require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-walker-nav-menu-edit.php' );
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-coming-soon.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-compatible.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-enqueue.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-extras.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-helper.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-import.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-init.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-instagram.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-metabox.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-tgm-plugin-activation.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-plugins.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-sercurity.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-templates.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-walker-nav-menu.php');
+require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-walker-nav-menu-edit.php');
 
-if ( class_exists( 'Redux' ) ) {
-	require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-redux.php' );
+if (class_exists('Redux')) {
+    require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-redux.php');
 }
 
-if ( class_exists( 'WooCommerce' )  ) {
-	require_once( LEZADA_FRAMEWORK_DIR . DS . 'class-woo.php' );
+if (class_exists('WooCommerce')) {
+    require_once(LEZADA_FRAMEWORK_DIR . DS . 'class-woo.php');
 }
 
 /**
  * Get option from Theme Options
  */
-if ( ! function_exists( 'lezada_get_option' ) ) {
-	function lezada_get_option( $option ) {
-		return Lezada_Helper::get_option( $option );
-	}
+if (!function_exists('lezada_get_option')) {
+    function lezada_get_option($option)
+    {
+        return Lezada_Helper::get_option($option);
+    }
 }
 
 
 // only run if Polylang is loaded
-if ( function_exists('pll_languages_list') ) {
-	add_action('wpml_loaded', '__return_true', 10, 0);
-	do_action('wpml_loaded');
+if (function_exists('pll_languages_list')) {
+    add_action('wpml_loaded', '__return_true', 10, 0);
+    do_action('wpml_loaded');
 }
 
-add_action('init', function() {
+add_action('init', function () {
     pll_register_string('breadcrumb-home', 'Home');
 
     pll_register_string('lk-login', 'Login');
@@ -93,6 +94,14 @@ add_action('init', function() {
     pll_register_string('lk-no-account', 'If you don’t have an account, register now!');
     pll_register_string('lk-username', 'Username');
     pll_register_string('lk-email', 'Email address');
+
+    pll_register_string('js-back', 'Back to top');
+    pll_register_string('js-wishlist', 'Browse Wishlist');
+    pll_register_string('js-added-to-wishlist', 'Product has been added to wishlist!');
+    pll_register_string('js-view-cart', 'View Cart');
+    pll_register_string('js-added-to-cart', 'has been added to cart!');
+    pll_register_string('js-product-added-to-cart', 'Product has been added to cart!');
+    pll_register_string('js-adding', 'Adding to cart...');
 
     pll_register_string('product-quantity', 'Quantity');
     pll_register_string('product-category', 'Category:');
